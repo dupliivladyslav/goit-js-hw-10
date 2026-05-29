661c066624a66eaf16b4665e4845d1c28242ea6a
dupliivladyslav <0204dvv@gmail.com>
2026-05-29 14:26:08 +0300

    add fixed js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -5,6 +5,7 @@ import 'izitoast/dist/css/iziToast.min.css';
 
 let selectedDate = null;
 const btn = document.querySelector('[data-start]');
+const input = document.querySelector('#datetime-picker');
 
 btn.disabled = true;
 
@@ -32,27 +33,34 @@ flatpickr('#datetime-picker', options);
 
 btn.addEventListener('click', () => {
   btn.disabled = true;
+  input.disabled = true;
 
   const interval = setInterval(() => {
     const remaining = selectedDate - Date.now();
-    if (remaining <= 0)
-      return (clearInterval(interval), updateDisplay(0, 0, 0, 0));
+
+    if (remaining <= 0) {
+      clearInterval(interval);
+      updateDisplay(0, 0, 0, 0);
+      input.disabled = false;
+      return;
+    }
+
     const { days, hours, minutes, seconds } = convertMs(remaining);
     updateDisplay(days, hours, minutes, seconds);
   }, 1000);
 });
 
 function convertMs(ms) {
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-  const days = Math.floor(ms / day);
-  const hours = Math.floor((ms % day) / hour);
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  return { days, hours, minutes, seconds };
+  const s = 1000,
+    m = s * 60,
+    h = m * 60,
+    d = h * 24;
+  return {
+    days: Math.floor(ms / d),
+    hours: Math.floor((ms % d) / h),
+    minutes: Math.floor((ms % h) / m),
+    seconds: Math.floor((ms % m) / s),
+  };
 }
 
 function updateDisplay(days, hours, minutes, seconds) {
